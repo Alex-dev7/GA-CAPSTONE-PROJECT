79d2e6f0f83d06475c9c70891435a1eb83976a88
Alexei Rusu <rusualexei1995@gmail.com>
2023-03-21 21:31:54 -0400

    get route - works

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,42 @@
+import Link from "next/link"
 
 
-// async function getData() {
+async function getData() {
+    const res = await fetch(`${process.env.BASE_URL}/api/post`, { cache: "no-cache" });
 
-//     const res = await fetch(`${process.env.BASE_URL}/api/hello`, { cache: "no-cache" });
+    const response  = await res.json();
+    // console.log(response)
+    return response
    
-//     const response  = await res.json();
-//     // console.log(response)
-  
-//     return response
-   
-// }
+}
 
 
 
 
 export default async function Home() {
 
-//  const posts = await getData()
+ const posts = await getData()
  
     
   return (
     <>
 
     <main className="py-8 px-48">
-        <h1>@ts-expect-error Async Server Component</h1>
+
+    <h1>---- HOME PAGE ----</h1>
      
-      
-       {/* {posts?.map((post )=> (
+      {/* @ts-expect-error Async Server Component */}
+       {posts?.map((post )=> (
         <div  key={post.id}>
         <Link  href={{pathname: `/post/${post.id}`}} className="text-lg py-6">
           {post.title}
         </Link>
         <br/>
-          <Delete className='w-8 h-9' id={post?.id} />     
+          {/* <Delete className='w-8 h-9' id={post?.id} />      */}
         <br/>        
         </div>
 
-       ))} */}
+       ))}
     </main>    
     
     </>
